504eb72b6f2d9ed5495c286f827fe1b4dcc24f3a
topgnikita <nikita.kerps@gmail.com>
2023-11-21 17:45:28 +0100

    nach uebung 4 zuhause

--- src/org/hbrs/se1/ws23/uebung4/view/UserStoryView.java
+++ src/org/hbrs/se1/ws23/uebung4/view/UserStoryView.java
@@ -7,9 +7,16 @@ import java.util.List;
 
 public class UserStoryView {
     public void dump(List<UserStory> list){
+        // Stream-basierte Implementierung mit Filter, Map und Collect
+        list.stream()
+                .sorted((us1, us2) -> Double.compare(us2.getPrio(), us1.getPrio())) // Sortiere nach Priorität absteigend
+                .forEach(us -> System.out.println(us.toString()));
+
+        /*
         Collections.sort(list);
         for (UserStory us : list) {
             System.out.println(us.toString());   //Ausgabe auf der Konsole
-        }
+        }*/
     }
+
 }
